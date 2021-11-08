b30f82ce70fe5c7bfc740b18d500b9e0e5938e07
Okutabu <67596669+Okutabu@users.noreply.github.com>
2021-11-08 18:08:38 +0100

    o

---
+++ accueil.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+        <div class="row">
+            <div class="col-1">
+                <h2>La montre aux super-pouvoirs:</h2>
+                <h3>la Rolex GMT-Master Batman</h3>
+            </div>
+            <div class="col-2">
+                    <img src="images/batman2.png" class="batman">
+                    <div class="color-box"></div>
+            </div>
+        </div>
+    </div>
+
+</body>
--- index.php
+++ index.php
@@ -1,38 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>OGEM</title>
-</head>
-<body>
-    <div class="container">
-        <div class="navbar">
-            <img src="images/good.png" class="logo">
-            <nav>
-                <ul>
-                    <li><a href="">Rolex</a></li>
-                    <li><a href="">Audemar Piguet</a></li>
-                    <li><a href="">Richard Mille</a></li>
-                    <li><a href="">Tudor</a></li>
-                </ul>
-            </nav>
-        </div>
-    
+<?php
 
-        <div class="row">
-            <div class="col-1">
-                <h2>La montre aux super-pouvoirs:</h2>
-                <h3>la Rolex GMT-Master Batman</h3>
-            </div>
-            <div class="col-2">
-                    <img src="images/batman2.png" class="batman">
-                    <div class="color-box"></div>
-            </div>
-        </div>
-    </div>
+$page = "acceuil";
+if (isset($_GET["page"]))
+    $page = $_GET["page"];
 
-</body>
-</html>
+    include "header.php";
+    include $page . ".php";
+    include "footer.php";
+
+
+?>
+
