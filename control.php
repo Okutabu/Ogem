7d9e179d7fe96566ac7a5beaecc7b3709a004b5d
Okutabu <67596669+Okutabu@users.noreply.github.com>
2021-11-11 23:28:34 +0100

    Update control.php

--- control.php
+++ control.php
@@ -5,7 +5,7 @@ function display_watch(){
     // in row container
     global $result;
     while($row = mysqli_fetch_assoc($result)){
-        echo "<div class='col-lg-3'>";
+        echo "<div ";
         echo "<img src='./images/".$row["user"]."_".$row["brand"]."_".$row["name"]."_image"."png>";
         echo "</div>";
         }
