1f9afa49aeca2d22b857ae0920d0ddbffc1bfdc5
Champagne-Champomy <82521444+Champagne-Champomy@users.noreply.github.com>
2021-11-15 17:03:09 +0100

    r

--- home.php
+++ home.php
@@ -1,18 +1,12 @@
 <main id="container">
-    <div class="row">
-            <div class="col-1">
-                <h2>La montre aux super-pouvoirs:</h2>
-                <h3>la Rolex GMT-Master Batman</h3>
-            </div>
-            <div class="col-2">
-                    <img src="images/batman2.png" class="batman">
-                    <div class="color-box"></div>
-            </div>
+    <article class="row">
+        <div class="col-1">
+            <h2>La montre aux super-pouvoirs:</h2>
+            <h3>la Rolex GMT-Master Batman</h3>
         </div>
-
-    <div class="container-fluid watch_display_container">
-        <div class="row red">
-        <?php
-        display_watch();
-        ?>
+        <div class="col-2">
+            <img src="images/batman2.png" class="batman">
+            <div class="color-box"></div>
+        </div>
+    </article>
 </main>
