210246a2b7eb952816fbddc90f9ab38721f3538a
Champagne-Champomy <82521444+Champagne-Champomy@users.noreply.github.com>
2021-11-15 17:09:47 +0100

    g

--- functions.php
+++ functions.php
@@ -1,20 +1,13 @@
 <?php
 
 function display_watch(){
+    //future fonction qui affiche les montres, a modif
 
     global $result;
     while($row = mysqli_fetch_assoc($result)){
         echo "<div class='col-lg-3'>";
         echo "<img src='./images/".$row["user"]."_".$row["brand"]."_".$row["name"]."_image"."png'>";
         echo "</div>";
-    }
-}
-
-
-function display_images($folder){
-    foreach($folder as $image){
-        if($image != ".." && $image !="."){
-        echo "<img src=./images/".$image." width='500px' alt=\"Une image qu'on aime bien\" >";}
     }
 }
 
