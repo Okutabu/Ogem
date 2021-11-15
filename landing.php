4f8655e3b53d9683c11820bf462fd7c82e8c0307
Champagne-Champomy <82521444+Champagne-Champomy@users.noreply.github.com>
2021-11-15 15:35:25 +0100

    rangement

--- landing.php
+++ landing.php
@@ -11,30 +11,4 @@
     $req = $bdd->prepare('SELECT * FROM utilisateurs WHERE token = ?');
     $req->execute(array($_SESSION['user']));
     $data = $req->fetch();
-    
-    
-
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div id="landdiv">
-        <img src="images/good.png" id="landimg" >
-        <?php  echo('<h1> Bonjour '.$data['pseudo'].' vous êtes bien connectez</h1>'); ?>
-        <a href="index.php">page d'acceuil</a>
-    </div>    
-    <footer id="landfoot">
-            &copy; OGEM 21
-    </footer>
-</body>
-
-</html>
