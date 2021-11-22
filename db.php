5451925612350f1370db74980160adeec0d5b302
Champagne-Champomy <82521444+Champagne-Champomy@users.noreply.github.com>
2021-11-22 16:31:14 +0100

    range

--- db.php
+++ db.php
@@ -1,14 +1,5 @@
 <?php
-    // Comprend po
-    try
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=ogem;charset=utf8', 'root', '');
-    } catch(PDOException $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
-
-    $connect = mysqli_connect("localhost", "ogem", "root", "");
+    $connect = mysqli_connect("localhost", "scgllydo_main", "root", "");
     mysqli_set_charset($connect, "utf8");
 
     $sql = "select * from watches";
