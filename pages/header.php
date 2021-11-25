348bed55897aefb2f01fa95d1d5c3e6ddb6304a8
Champagne-Champomy <82521444+Champagne-Champomy@users.noreply.github.com>
2021-11-25 23:28:38 +0100

    resol bugs

--- pages/header.php
+++ pages/header.php
@@ -6,7 +6,7 @@
         <li><a href=".?page=home">Accueil</a></li>
         <li><a href=".?page=search">Montres à vendre</a></li>
         <li><a href=".?page=sell">Vendre</a></li>
-        <li><a href=".?page=sign">Connexion/Inscription</a></li>
+        <li><a href=".?page=connexion">Connexion/Inscription</a></li>
         <!-- Mettre photo de profil ci-dessous et quand on clique menu déroulant avec profil, vendre, panier et se déconnecter -->
         <li><p>Profil</p></li> 
     </ul>
