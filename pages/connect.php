61c97acc41791e0294d3244dca8bcc583881aa20
Champagne-Champomy <gaetan.ignarra.pro@gmail.com>
2021-11-27 11:28:42 +0100

    insc connect

--- pages/connect.php
+++ pages/connect.php
@@ -1,10 +1,11 @@
 <main class="login-form">
     <h2 class="text-center">Connectez-vous</h2>
-    <form action="../index.php" method="post">
+    <form action="." method="post">
         <input type="hidden" name="action" value="connexion">
         <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
         <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
         <input type="submit" class="btn" value="Connexion">
     </form>
-    <a href=""></a>
+    <p>Pas encore de compte ?</p>
+    <a href=".?page=inscription">Inscrivez-vous</a>
 </main>
--- pages/inscription.php
+++ pages/inscription.php
@@ -1,6 +1,6 @@
 <main class="login-form">
     <h2 class="text-center">Inscription</h2>
-    <form action="../index.php" method="post">
+    <form action="." method="post">
         <input type="hidden" name="action" value="inscription">
         <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
         <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
