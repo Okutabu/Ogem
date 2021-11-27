6efb64f02b9a91b5cb0b0d5748ef84c64bd4f80d
Champagne-Champomy <gaetan.ignarra.pro@gmail.com>
2021-11-27 11:32:21 +0100

    insc

--- pages/inscription.php
+++ pages/inscription.php
@@ -3,6 +3,7 @@
     <form action="." method="post">
         <input type="hidden" name="action" value="inscription">
         <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
+        <input type="text" name="email" class="form-control" placeholder="Adresse mail" required="required" autocomplete="off">
         <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
         <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
         <input type="submit" class="btn" value="Inscription">
